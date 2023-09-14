17c68dc1aafd06e1b7a1f7d5450d7aeb4cdc3988
Md Abdul Aziz <abdulazizwebdeveloper1996@gmail.com>
2023-09-14 07:13:39 +0600

    middleware security add

--- app.js
+++ app.js
@@ -9,6 +9,43 @@ const router = require('./src/routes/api');
 
 
 
+// npm i nodemon express body-parser cookie-parser multer jsonwebtoken mysql mongoose dotenv cors express-mongo-sanitize express-rate-limit helmet hpp validator
+
+//All security Middleware Import
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const multer = require('multer');
+const jsonwebtoken = require('jsonwebtoken');
+const mysql = require('mysql');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const expressMongoSanitize = require('express-mongo-sanitize');
+const expressRateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const hpp = require('hpp');
+const validator = require('validator');
+
+
+// Security Middleware Implement
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+app.use(helmet());
+app.use(hpp());
+
+
+// Requset Security Rate Limiting
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+
+});
+
+
+// Apply the Security rate limiting middleware to all requests
+app.use(limiter);
+
 
 // Create Api router paths
 app.use('/api',router);
